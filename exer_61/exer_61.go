adef41d6f6af173b8c152be9249fd71237a7bd12
JesusOh <ojsboy@hotmail.com>
2016-06-29 07:05:10 +0900

    happy

--- exer_61/exer_61.go
+++ exer_61/exer_61.go
@@ -10,8 +10,24 @@ type rot13Reader struct {
 	r io.Reader
 }
 
-func (r *rot13Reader) Read(p []byte) (n int, err error) {
+func rot13Calc(p byte) byte {
+	if (p >= 'a' && p <= 'm') || (p >= 'A' && p <= 'M') {
+		p += 13
+	} else if (p >= 'n' && p <= 'z') || (p >= 'N' && p <= 'Z') {
+		p -= 13
+	}
 
+	return p
+}
+
+func (r *rot13Reader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+
+	for i := 0; i < n; i++ {
+		p[i] = rot13Calc(p[i])
+	}
+
+	return
 }
 
 func main() {
