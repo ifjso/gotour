64659efbb9ea498bd7b20171049607829eef66a4
JesusOh <ojsboy@hotmail.com>
2016-07-08 07:02:15 +0900

    repeat 1

--- gotour_exercise-slices/exercise-slices.go
+++ gotour_exercise-slices/exercise-slices.go
@@ -4,14 +4,12 @@ import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
 	pic := make([][]uint8, dy)
-
 	for y := range pic {
 		pic[y] = make([]uint8, dx)
-
 		for x := range pic[y] {
-			pic[y][x] = uint8(x ^ y)
-			// pic[y][x] = uint8((x + y) / 2)
-			// pic[y][x] = uint8(x * y)
+			pic[y][x] = uint8((x + y) / 2)
+			// pic[y][x] = x * y
+			// pic[y][x] = x ^ y
 		}
 	}
 
